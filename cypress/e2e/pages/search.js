7ad7b22868b1b358acd75f16b83c2eb6a1afe72a
Muhamad Iqbal <muhmdiqbl12@gmail.com>
2024-11-25 08:45:40 +0700

    Menambahkan script baru search.js

--- cypress/e2e/pages/search.js
+++ cypress/e2e/pages/search.js
@@ -1,15 +1,16 @@
+import SearchPage from './search.page';
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given("visit the website login page webappsecurity", () => {
-    cy.visit("http://zero.webappsecurity.com/login.html");
+    SearchPage.visit();
 });
 
 When("the user enters a valid username and password", () => {
-    cy.get("#user_login").type("username");
-    cy.get("#user_password").type("password");
-    cy.get('[type="submit"]').click();
+   SearchPage.fillUsername();
+   SearchPage.fillPassword();
+   SearchPage.SIGN_IN();
 });
 
 Then("find the search button", () => {
-    cy.get("#searchTerm").should("be.visible");
+ SearchPage.SEARCHFEATURE()
 });

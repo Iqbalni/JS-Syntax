02ac10123496ea095a5963645f9f7e22b1219e3d
Muhamad Iqbal <muhmdiqbl12@gmail.com>
2024-12-16 06:28:08 +0700

    revisi tugas headers

--- cypress/e2e/integration/pokeapi/headers.test.cy.js
+++ cypress/e2e/integration/pokeapi/headers.test.cy.js
@@ -1,30 +1,23 @@
-describe('Validate Header',()=>{
-    it('Succesfully  validate content-type',()=>{
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('content-type').should('include', 'application/json; charset=utf-8')
+describe('Validate Pokemon Abilities API',()=>{
+   it('Validates the response body of abilities', () => {
+    cy.request({
+                    method: 'GET',
+                    url:'https://pokeapi.co/api/v2/pokemon/ditto',
     })
-    it('Succesfully validate Content-Length',()=> {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('content-length').should('include', '2023')
+    .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.status).equal(200);
+        expect(response.body.abilities[0].ability.name).to.eq('limber')
+        expect(response.body.abilities[0].ability.url).to.eq('https://pokeapi.co/api/v2/ability/7/')
+
+        expect(response.body.abilities[1].ability.name).to.eq('imposter')
+        expect(response.body.abilities[1].ability.url).to.eq('https://pokeapi.co/api/v2/ability/150/')
+
+
+        expect(response.body.cries.latest).to.eq('https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/132.ogg')
+
+        expect(response.body.forms[0].name).to.eq('ditto')
+        expect(response.body.forms[0].url).to.eq('https://pokeapi.co/api/v2/pokemon-form/132/')
     })
-    it('Succesfully validate Connection',()=> {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('connection').should('include', 'keep-alive')
-    })
-    it('Succesfully validate Cache-Control',()=> {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('cache-control').should('include', 'public, max-age=86400, s-maxage=86400')
-    })
-        it('Succesfully validate etag',()=> {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('etag').should('include', 'W/"5e0d-5fWRaxlbpjY4eNJNjWKLzaZ/amE"')
-    })
-        it('Succesfully validate strict-transport-security',()=> {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('strict-transport-security').should('include', 'max-age=31556926')
-    })
-     it('Succesfully validate x-country-code',()=> {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('x-country-code').should('include', 'US')
-    })
+   })
 })
